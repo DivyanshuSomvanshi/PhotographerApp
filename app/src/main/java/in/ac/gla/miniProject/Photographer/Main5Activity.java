c487ddf3e3a605d51deb9234b3c77edae56571a1
DivyanshuSomvanshi <divyanshu.singhraw705@gmail.com>
2019-11-23 00:07:23 -0800

    final commit

---
+++ app/src/main/java/in/ac/gla/miniProject/Photographer/Main5Activity.java
@@ -0,0 +1,45 @@
+package in.ac.gla.miniProject.Photographer;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Main5Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main5);
+        Boolean isFirstTime;
+
+
+        SharedPreferences app_preferences = PreferenceManager
+                .getDefaultSharedPreferences(Main5Activity.this);
+
+
+        SharedPreferences.Editor editor = app_preferences.edit();
+
+        isFirstTime = app_preferences.getBoolean("isFirstTime", true);
+
+
+        if (isFirstTime) {
+
+            Intent intent = new Intent(Main5Activity.this, Login.class);
+            startActivity(intent);
+
+//implement your first time logic
+            editor.putBoolean("isFirstTime", false);
+            editor.commit();
+
+        } else {
+
+            Intent intent = new Intent(Main5Activity.this, Main3Activity.class);
+            startActivity(intent);
+//app open directly
+        }
+    }
+}
+
--- app/src/main/java/in/ac/gla/miniProject/Photographer/SplashScreen.java
+++ app/src/main/java/in/ac/gla/miniProject/Photographer/SplashScreen.java
@@ -1,10 +1,10 @@
 package in.ac.gla.miniProject.Photographer;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreen extends AppCompatActivity {
     Handler handler;
@@ -17,7 +17,7 @@ public class SplashScreen extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent=new Intent(SplashScreen.this,Login.class);
+                Intent intent = new Intent(SplashScreen.this, Main5Activity.class);
                 startActivity(intent);
                 finish();
             }
